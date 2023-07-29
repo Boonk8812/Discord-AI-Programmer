42ca27c16adfe4919cc1217fccbf060fc10791a0
Boonk3312 <111024718+Boonk8812@users.noreply.github.com>
2023-07-29 09:14:07 -0700

    Update info.h

--- info.h
+++ info.h
@@ -1,4 +1,5 @@
 "version": [0.0.1]
   "guilder-version": [2.7.1]
     "name": ["Copilot Programmer AI"]
-     "display-name": ["copilot_programmer-6882]    
+     "display-name": ["copilot_programmer-6882"]    
+       "id": ["132290663820481442"]
