56a5b467750497083f72cdf09f0405448adb9e5f
Boonk3312 <111024718+Boonk8812@users.noreply.github.com>
2023-07-31 16:56:42 -0700

    Update 0a$00.py

--- .classes/0000.py
+++ .classes/0000.py
@@ -1,1 +1,17 @@
 print("0xF3BCFE41BBBB0D3EBFF4FF3EA02BA5E3")
+
+#md5ChecksumGen 
+class MD5 Checksum Generator
+
+# Generate a MD5 Checksum in the following format: 0X00-XXXX-0-9-XXX
+
+
+print("0x0 + Generated Checksum")
+
+convert("Generated Checksum to a 32-bit 6-digit number string")
+
+function(("$00 $0A $5A");
+def inJack
+
+inJack("start // ? 5e1a0-0bc11-b5cee-FFFF5-0cf4e-00e31 \\ ?")
+
