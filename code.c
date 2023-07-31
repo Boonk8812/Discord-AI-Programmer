8fecfcec29c9667ebfd569a6b640838d22adfa03
Boonk3312 <111024718+Boonk8812@users.noreply.github.com>
2023-07-30 21:41:23 -0700

    Create code.c

---
+++ code.c
@@ -0,0 +1,28 @@
+std::cout("ERROR 666: bot - ERR666 - Glitch666F: Hack0=%÷×[$;31[$%<×[:<%×"÷8<×"8<%×÷"÷9+<"%[÷×<"÷[<"%÷>+";÷×9×;"÷")
+  std::cout(": version :")
+   std::cout("Bot version 0.0.0-01")
+    std::cout("rundll32.exe crashed with a unknown exception. (0x04748579173b6)")
+
+(
+  // Export Assembly Code
+
+  
+  assembly.chr('0f661b00d3ecc4a11b');
+code.id("F3BC FE41 FE41 BBBB");
+command1.cmdLineStringKey
+
+  export C:/WINDOWS
+   import GLFW
+  
+STD::STRING(); 0.0.0: 0.0.0.0
+
+  stich.DATA("4cee31d720b82fee4170a618ffe4")
+ define[link]:("http://discord.com")
+
+
+
+  (
+
+  "OS": ['Windows Server 2008 R2 Data Center Edition ']
+
+  uv.LightTextForProfile("#50C878")
